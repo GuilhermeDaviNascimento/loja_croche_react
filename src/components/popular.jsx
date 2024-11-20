a63eb713a619f2c7ecf51d40fcb36928276c42d2
GuilhermeDaviNascimento <131400280+GuilhermeDaviNascimento@users.noreply.github.com>
2024-11-20 17:33:20 -0300

    a

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -3,22 +3,16 @@ import { Link } from "react-router-dom";
 export default function components({ id, nome, preco, foto }) {
   return (
     <Link className="bg-emphasis w-72 h-40 rounded-lg p-3 flex justify-center items-center shrink-0" to={`/roupa/${id}`}>
-      <div className="flex flex-col justify-between w-max h-full">
+      <div className="flex flex-col justify-center w-max h-full">
         <div className="flex items-center">
           <p className="text-default">5</p>
           <i class="fa-solid fa-star"></i>
         </div>
-
-        <div>
+        <div className="w-32">
           <h3 className="text-default">{nome}</h3>
           <h3 className="text-default">{preco} R$</h3>
         </div>
-        <div className="flex gap-3 items-center">
-          <p className="text-default">Cores</p>
-          <div className="w-5 h-5 bg-slate-300 rounded-full"></div>
-          <div className="w-5 h-5 bg-red-300 rounded-full"></div>
-          <div className="w-5 h-5 bg-green-300 rounded-full"></div>
-        </div>
+
       </div>
       <div className="relative inline-block">
         <img src={foto} alt="roupa1" className="relative z-10" />
